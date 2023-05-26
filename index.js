3b8881969e06d448168599541f8c82f88b50ac25
João Silva <109978656+JOao7640@users.noreply.github.com>
2023-05-26 15:11:19 +0100

    home page updates

---
+++ index.js
@@ -0,0 +1,13 @@
+let navLinks = document.getElementById("navLinks");
+        
+    function showMenu(){
+    
+        navLinks.style.right = "0";            
+    
+    }
+
+    function hideMenu(){
+        
+        navLinks.style.right = "-200px";
+        
+    }
